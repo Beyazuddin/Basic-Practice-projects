79945c8da7aafa1662eb2bfa967ae63574abaadf
Taif <taif.soldev@gmail.com>
2023-03-29 14:16:00 +0530

    worked on js

---
+++ Hex-color-generator/app.js
@@ -0,0 +1,15 @@
+const inputElement = document.querySelector(".inp");
+const buttonElement = document.querySelector(".btn");
+const h1Element = document.querySelector(".h1");
+const copyBtnElement = document.querySelector(".copy_btn");
+
+buttonElement.addEventListener("click", () => {
+    let hexNum = Math.random().toString(16).slice(2, 8);
+    const hexcolor = "#" + hexNum;
+    const inputElementColor = (inputElement.value = hexcolor);
+    document.body.style.backgroundColor = inputElementColor;
+    h1Element.style.color = inputElementColor;
+    copyBtnElement.style.color = inputElementColor;
+    copyBtnElement.style.backgroundColor = inputElementColor;
+});
+
